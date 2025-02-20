2101397c59d0920c4c8ca8ae4e440ff6e8f77d92
tshjustin <j.tann2305@gmail.com>
2025-02-20 07:08:37 +0000

    Updated Tailwind CSS

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ npm run server
 npm run dev 
 ```
 
-Vite: For fast development and optimized production builds
+`Vite`: For fast development and optimized production builds
 
 
-Express: Backend server for file I/O 
+`Express`: Backend server for file I/O 
 
 
-TypeScript + React: Building UI Components / Adds type definitions to data structures & ensures type safety in function parameter
+`React + TypeScript` : Building UI Components / Adds type definitions to data structures & ensures type safety in function parameter
 
 
-ShadCN / Lucid React / Tailwind: Styling 
+`ShadCN / Lucid React / Tailwind`: Styling 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-//   theme: {
-//     extend: {
-//       keyframes: {
-//         fadeIn: {
-//           '0%': { opacity: '0', transform: 'translateY(10px)' },
-//           '100%': { opacity: '1', transform: 'translateY(0)' }
-//         },
-//         buttonPress: {
-//           '0%, 100%': { transform: 'scale(1)' },
-//           '50%': { transform: 'scale(0.95)' }
-//         }
-//       },
-//       animation: {
-//         fadeIn: 'fadeIn 0.5s ease-out',
-//         buttonPress: 'buttonPress 0.2s ease-in-out',
-//       }
-//     },
-//   },
-//   plugins: [],
-// };
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
